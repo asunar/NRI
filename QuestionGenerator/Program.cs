a36c407428f73cf09db3a15510ed20ea402e0650
Alper Sunar <ASunar@Paylocity.com>
2018-08-05 14:39:21 -0500

    Try to put the same number of questions from each of the strand's standards into the quiz.

--- QuestionGenerator/Program.cs
+++ QuestionGenerator/Program.cs
@@ -12,6 +12,7 @@ namespace QuestionGenerator
     {
       //Input validation, error handling is ignored for now.      
       var questionCount = Convert.ToInt32(args[0]);
+      //var questionCount = 5;
       Console.WriteLine("Generating {0} questions...", questionCount);
       var strands = Populate();
       var questions = GenerateQuestions(questionCount, strands);
@@ -24,18 +25,37 @@ namespace QuestionGenerator
 
     private static List<Question> GenerateQuestions(int questionCount, List<Strand> strands)
     {
-      var questions = new List<Question>();
-      //First, try to put the same number of questions from each strand into the quiz.
+      var questions = new List<Question>();      
       for (var i = 0; i < questionCount; i++)
       {
-        var currentStrand = i % 2 == 0 ? strands[0] : strands[1]; // Refactor to improve strand selection     
-        var selectedQuestion = currentStrand.Standards.First().Questions.First();
+        var currentStrand = i % 2 == 0 ? strands[0] : strands[1]; // Refactor to improve strand selection
+
+
+
+        var selectedQuestion = PickQuestion(currentStrand, questions);
         questions.Add(selectedQuestion);
 
 
       }
 
       return questions;
+    }
+
+    private static Question PickQuestion(Strand currentStrand, List<Question> currentQuestions)
+    {
+//Second, try to put the same number of questions from each of the strand's standards into the quiz.
+      var firstStandardInCurrentStrand = currentStrand.Standards.First();
+      if (!currentQuestions.Any())
+      {
+        return firstStandardInCurrentStrand.Questions.First();
+      }
+
+      var existingStandardIds = currentQuestions.Select(x => x.StandardId);
+      var standardToPickFrom = currentStrand.Standards.FirstOrDefault(x => !existingStandardIds.Contains(x.ID)) ?? firstStandardInCurrentStrand;
+
+      var rnd = new Random();
+      var candidateQuestions = standardToPickFrom.Questions;
+      return candidateQuestions[rnd.Next(0, candidateQuestions.Count())];
     }
 
     public static List<Strand> Populate()
@@ -56,8 +76,8 @@ namespace QuestionGenerator
               Name = "Common Nouns",
               Questions = new List<Question>
               {
-                new Question {ID = 1, Difficulty = 0.7},
-                new Question {ID = 2, Difficulty = 0.6}
+                new Question {ID = 1, Difficulty = 0.7, StandardId = 1},
+                new Question {ID = 2, Difficulty = 0.6, StandardId = 1}
               }
             },
             new Standard
@@ -66,7 +86,7 @@ namespace QuestionGenerator
               Name = "Abstract Nouns",
               Questions = new List<Question>
               {
-                new Question {ID = 3, Difficulty = 0.8}
+                new Question {ID = 3, Difficulty = 0.8, StandardId = 2}
               }
             },
 
@@ -76,9 +96,9 @@ namespace QuestionGenerator
               Name = "Proper Nouns",
               Questions = new List<Question>
               {
-                new Question {ID = 4, Difficulty = 0.2},
-                new Question {ID = 5, Difficulty = 0.5},
-                new Question {ID = 6, Difficulty = 0.4}
+                new Question {ID = 4, Difficulty = 0.2, StandardId = 3},
+                new Question {ID = 5, Difficulty = 0.5, StandardId = 3},
+                new Question {ID = 6, Difficulty = 0.4, StandardId = 3}
               }
             }
           }
@@ -97,8 +117,8 @@ namespace QuestionGenerator
               Name = "Action Verbs",
               Questions = new List<Question>
               {
-                new Question {ID = 7, Difficulty = 0.9},
-                new Question {ID = 8, Difficulty = 0.1}
+                new Question {ID = 7, Difficulty = 0.9, StandardId = 4},
+                new Question {ID = 8, Difficulty = 0.1, StandardId = 4}
               }
             },
 
@@ -108,9 +128,9 @@ namespace QuestionGenerator
               Name = "Transitive Verbs",
               Questions = new List<Question>
               {
-                new Question {ID = 9, Difficulty = 0.3},
-                new Question {ID = 10, Difficulty = 0.6},
-                new Question {ID = 11, Difficulty = 0.4}
+                new Question {ID = 9, Difficulty = 0.3, StandardId = 5},
+                new Question {ID = 10, Difficulty = 0.6, StandardId = 5},
+                new Question {ID = 11, Difficulty = 0.4, StandardId = 5}
               }
             },
 
@@ -120,7 +140,7 @@ namespace QuestionGenerator
               Name = "Reflexive Verbs",
               Questions = new List<Question>
               {
-                new Question {ID = 12, Difficulty = 0.2}
+                new Question {ID = 12, Difficulty = 0.2, StandardId = 6}
               }
             }
           }
@@ -150,5 +170,6 @@ namespace QuestionGenerator
   {
     public int ID { get; set; }
     public double Difficulty { get; set; }
+    public int StandardId { get; set; }
   }
 }
